58ffc695feb24dc680ebf3e727f75516c7d81024
Ricardo <ochor01@pfw.edu>
2021-03-05 11:11:09 -0500

    Add project files.

---
+++ DesignAStack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignAStack
+{
+    class Program
+    {
+        private object[] stack;
+        private int top;
+
+        public void Push(object obj) {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null");
+            }
+            else {
+                stack[++top] = obj;
+            }
+
+         
+            
+        }
+        public object Pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("stack is empty");
+                return -1;
+            }
+            else {
+                return stack[top--];
+            }
+
+        }
+        public void Clear() {
+            for (int i=0; i <= stack.Length ; i++) {
+                Console.WriteLine(stack[--top]);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Stack<object> mystack = new Stack<object>();
+            mystack.Push(1);
+            mystack.Push(2);
+            mystack.Push(3);
+
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine(mystack.Pop());
+
+
+
+        }
+    }
+}
