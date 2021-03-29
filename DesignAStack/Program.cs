a97577f7b8ae8c0e8551f78f455e1ca817b6df86
Ricardo <ochor01@pfw.edu>
2021-03-29 16:57:58 -0400

    Adding a min and max

--- DesignAStack/Program.cs
+++ DesignAStack/Program.cs
@@ -10,14 +10,26 @@ namespace DesignAStack
 
         static void Main(string[] args)
         {
-            Stack<object> mystack = new Stack<object>();
-            mystack.Push(1);
+            var mystack = new Stack<object>();
+            /*mystack.Push(1);
             mystack.Push(2);
             mystack.Push(3);
 
             Console.WriteLine(mystack.Pop());
             Console.WriteLine(mystack.Pop());
-            Console.WriteLine(mystack.Pop());
+            Console.WriteLine(mystack.Pop());*/
+
+            var start = 0;
+            var end = 5;
+
+            for (int i = start; i > end; i++) {
+                mystack.Push(i);
+            }
+
+            for (int i = start; i > end; i++) {
+                mystack.Pop();
+            }
+
         }
 
         public void Push(object obj) {
