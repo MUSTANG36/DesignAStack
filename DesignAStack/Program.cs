30b0837a4a1ea0c3e2f0d0da5d0a7abb4caba6df
Ricardo <ochor01@pfw.edu>
2021-03-30 11:54:58 -0400

    new class

--- DesignAStack/Program.cs
+++ DesignAStack/Program.cs
@@ -5,9 +5,7 @@ namespace DesignAStack
 {
     class Program
     {
-        private object[] stack;
-        private int top;
-
+   
         static void Main(string[] args)
         {
             var mystack = new Stack<object>();
@@ -27,41 +25,12 @@ namespace DesignAStack
             }
 
             for (int i = start; i > end; i++) {
-                mystack.Pop();
+               Console.WriteLine(mystack.Pop());
             }
-
-        }
-
-        public void Push(object obj) {
-            if (obj == null)
-            {
-                throw new InvalidOperationException("Null");
-            }
-            else {
-                stack[++top] = obj;
-            }
-
-         
             
         }
-        public object Pop()
-        {
-            if (top == -1)
-            {
-                Console.WriteLine("stack is empty");
-                return -1;
-            }
-            else {
-                return stack[top--];
-            }
 
-        }
-        public void Clear() {
-            for (int i=0; i <= stack.Length ; i++) {
-                Console.WriteLine(stack[--top]);
-            }
-
-        }
+        
        
     }
 }
---
+++ DesignAStack/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignAStack
+{
+    public class Stack
+    {
+        // we don't want to re-initialize that list anywhere else in the class
+        private readonly List<Object> stack = new List<Object>();
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null");
+            }
+            else
+            {
+                stack.Add(obj);
+                //stack[++top] = obj;
+            }
+
+
+
+        }
+        public object Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException("There's no new elements");
+                
+            }
+
+            var top = stack.Count - 1;
+            var index = stack[top];
+
+            stack.RemoveAt(top);
+
+            return index;
+
+
+        }
+        public void Clear()
+        {
+            stack.Clear();
+       
+
+        }
+    }
+}
